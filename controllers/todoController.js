b0b31ee5a29b0f62274ce27642b4240e4aef3617
Casaemiro <silasmagho@gmail.com>
2022-09-15 11:50:01 +0100

    todo App linked to mongodb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,28 +1,49 @@
 var bodyParser = require('body-parser')
-var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: "kick some coding ass" }]
+var mongoose = require('mongoose');
+//connect to the database
+mongoose.connect('mongodb+srv://test:test@cluster0.lkuvbdp.mongodb.net/test')
+//This is llike a blue print
+
+
+
+var todoSchema = new mongoose.Schema({
+    item: String
+})
+
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+
+
+// var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: "kick some coding ass" }]
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 module.exports = function (app) {
     // console.log(app);
 
     app.get('/todo', function (req, res) {
-        res.render('todo', { todos: data })
+        Todo.find({}, function(err, data){
+            if(err) throw err;
+            res.render('todo', { todos: data })
+        })
     });
 
     app.post('/todo', urlencodedParser, function (req, res) {
-        data.push(req.body);
-        console.log(data);
-        res.json(data);
+    // get data from view and add  it to mongodb
+    var newTodo = Todo(req.body).save(function(err, data){
+        if(err) throw err;
+        res.json(data);   
+    })
     });
 
     app.delete('/todo/:item', function (req, res) {
+        //delete the requested item from mongodb
         var test = req.params.item
         test = test.split("-").join(" ").trim().split(" ").join("-")
-        data = data.filter((todo)=>{
-            console.log(todo.item.split(" ").join("-"))
-            return todo.item.split(" ").join("-") !== test;
+        Todo.find({item: test.split("-").join(" ")}).remove(function(err, data){
+            if (err) throw err
+            res.json(data);
+        })
+       
         });
-        console.log(data)
-        console.log(test);
-        res.json(data);
-    });
+    
 }
